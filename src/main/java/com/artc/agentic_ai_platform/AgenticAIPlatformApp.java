4cb9dc7639236b07d7407424c69ef77ed4b69cff
Noopur Arora <noopurarora@Noopurs-MacBook-Pro.local>
2025-12-19 16:39:41 +0800

    Add spring boot application file.

---
+++ src/main/java/com/artc/agentic_ai_platform/AgenticAIPlatformApp.java
@@ -0,0 +1,13 @@
+package com.artc.agentic_ai_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenticAIPlatformApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgenticAIPlatformApp.class, args);
+    }
+
+}
